b8d6855eab701ca77576e186df9417dbb451b988
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-12-29 15:00:58 +0600

    Enhance home page layout with improved styling and background color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 const HomePage = () => {
   return (
-    <main className="flex justify-center items-center h-screen">
-      <h1 className="text-green-500 text-5xl font-bold">Home Page</h1>
+    <main className="flex justify-center items-center min-h-screen bg-sky-200">
+      <div className="w-full max-w-screen-2xl mx-auto p-20 bg-slate-100 rounded-2xl shadow-lg">
+        <h1 className="text-green-500 text-5xl font-bold">Home Page</h1>
+      </div>
     </main>
   );
 };
