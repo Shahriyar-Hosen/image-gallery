2bded571c3712d8b9ef082ef681d2c8aeb15cf4b
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-12-31 21:21:53 +0600

    Add Header component with search functionality and integrate it into the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/header";
 import { Navbar } from "@/components/nav";
 
 const HomePage = () => {
@@ -5,6 +6,7 @@ const HomePage = () => {
     <main className="flex justify-center items-center min-h-screen bg-sky-200">
       <div className="w-full max-w-screen-2xl mx-auto p-20 bg-slate-100 rounded-2xl shadow-lg">
         <Navbar />
+        <Header />
         <h1 className="text-green-500 text-5xl font-bold">Home Page</h1>
       </div>
     </main>
