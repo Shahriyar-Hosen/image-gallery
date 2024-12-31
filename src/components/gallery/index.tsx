0e4677f15b807579b770a96a324e83df6dbe835b
Shahriyar-Hosen <me.shahriyarhosen@gmail.com>
2024-12-31 22:07:38 +0600

    Filter gallery images by category based on search parameters

--- src/components/gallery/index.tsx
+++ src/components/gallery/index.tsx
@@ -1,10 +1,20 @@
+"use client";
+
 import { data } from "@/lib/db";
+import { useSearchParams } from "next/navigation";
 import Card from "./card";
 
 export const Gallery = () => {
+  const params = useSearchParams();
+  const category = params.get("category");
+
+  const images = data.filter((item) =>
+    category ? item.category === category : item
+  );
+
   return (
     <div className="pt-10 grid grid-cols-3 gap-6">
-      {data.map((item) => (
+      {images.map((item) => (
         <Card key={item.id} {...item} />
       ))}
     </div>
